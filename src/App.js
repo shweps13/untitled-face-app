547c4216c831c076a14544ee47e0c7d3129c7d79
Heorhii Siburov <g.siburov@gmail.com>
2021-01-02 18:38:15 -0800

    face-api models was imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
       </header>
     </div>
   );
