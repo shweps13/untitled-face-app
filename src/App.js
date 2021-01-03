01d26ac0912f88fea940769e4478275c3253f68c
Heorhii Siburov <g.siburov@gmail.com>
2021-01-02 18:51:50 -0800

    fixed problem with babel-loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Video from './components/Video.js'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Video />
     </div>
   );
 }
---
+++ src/components/Video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Video() {
+
+    return (
+    <div className="App-header">
+        <h3>Pew-pew</h3>
+    </div>
+    )
+}
+
+export default Video
