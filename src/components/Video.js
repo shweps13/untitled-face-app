d4f955bdeb9a9c639a9cecdf81866ca7b0a9d88f
Heorhii Siburov <g.siburov@gmail.com>
2021-01-02 19:01:00 -0800

    preparations for video import

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import * as faceapi from 'face-api.js';
 
 function Video() {
 
+    const [init, setInit] = useState(false);
+    const videoRef = useRef();
+    const canvasRef = useRef();
+
+    let videoW = 480;
+    let videoH = 640;
+
+
     return (
     <div className="App-header">
-        <h3>Pew-pew</h3>
+        <span>{init ? 'Initializing' : 'Ready'}</span>
+        <video ref={videoRef} autoPlay muted width={videoW} height={videoH} />
+        <canvas ref={canvasRef} />
     </div>
     )
 }
